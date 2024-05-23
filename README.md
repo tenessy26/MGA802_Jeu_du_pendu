50ab026268f3696d6db1c3afd6b14865653ead52
tenessy_de-faria <tenessy.de-faria@knap.fr>
2024-05-23 09:48:20 -0400

    Version finale

---
+++ README.md
@@ -0,0 +1,14 @@
+ Le programme python est un jeu du pendu. Les biliothèques nécessaires à l'exécution du programme
+ sont les suivantes : 'random', 'unicodedata', 'os'
+ Le nombre de chance au début de chaque partie est de 6 par défault, ce paramètre est modifiable
+ à l'appel de la fonction "lancer_jeu()". (dernière ligne du programme). L'utilisateur à le choix 
+ d'entrer son propre fichier de mot, pour cela, il suffit 'ajouter le fichier ".txt" avec un mot 
+ par ligne dans le dossier du projet.
+ A chaque tour, l'utilisateur a le choix d'entrer une lettre, si la lettre ne fait pas partie du mot à 
+ trouver, le joueur perd une chance. Si la lettre est correcte, l'état du mot change et l'emplacement 
+ de la lettre entrée est affichée. La partie continue jusqu'à ce que le joueur trouve toutes les lettres 
+ du mot ou jusqu'à ce que son nombre de chance restante soit épuisée. L'utiliateur peut ensuite faire le
+ choix de relancer une partie en appuyant sur "entrer".
+ 
+ 
+ 
--- script_jeu.py
+++ script_jeu.py
@@ -1,63 +1,59 @@
+# JEU DU PENDU
+
 import random
 import os
 from unicodedata import normalize
 
-def selectionner_mot():
+def selectionner_mot():             # Fonction de sélection dun mot soit dans un fichier externe ou un fichier par défault
 
-    external_file = input(" Quel est le nom du fichier ? ")
-    chemin_complet = os.path.join(os.path.abspath(os.path.curdir), external_file)
-    print(chemin_complet)
-
-    print(os.path.isfile(chemin_complet))
-    if os.path.isfile(chemin_complet):
-        word_file = open("mots.txt",'r', encoding='utf8')
-        word_array = word_file.readlines()
+    external_file = input("\nSi vous souhaitez utiliser un fichier de mot externe, ajoutez le au dossier du projet et spécifiez son nom, sinon appuyez sur 'entrer' :\n ")
+    chemin_complet = os.path.join(os.path.abspath(os.path.curdir), external_file)   # Détermination du chemin complet jusqu'au fichier externe
+    if os.path.isfile(chemin_complet):       # Vérification de l'existance du fichier
+        word_file = open(chemin_complet,'r', encoding='utf8')
+        word_array = word_file.read().splitlines()
         word = random.choice(word_array)
-        word = word[:-1]
         word = normalize('NFD',word).encode('ASCII','ignore').decode('utf8')
     else:
         word_file = open("mots.txt",'r', encoding='utf8')
-        word_array = word_file.readlines()
+        word_array = word_file.read().splitlines()
         word = random.choice(word_array)
-        word = word[:-1]
         word = normalize('NFD',word).encode('ASCII','ignore').decode('utf8')
-        print(word)
     return word
 
-def afficher_etat_mot(word):
+def afficher_etat_mot(word):  # Fonction affichant l'état d'un mot
     mot = ""
-    for l in word:
+    for l in word:            # On boucle sur chaque lettre du mot pour afficher les lettres trouvées
         mot += l
     print(f" Voici l'état du mot a deviner : {mot}")
     return mot
-def verifier_lettre(lettre,mot,word_state,nombre_de_chances):
+def verifier_lettre(lettre,mot,word_state,nombre_de_chances):  # Fonction vérifiant la lettre entrée
 
     bool = False
-    for l in range(len(mot)):
+    for l in range(len(mot)):  # On boucle sur chaque lettre du mot pour vérifier si la lettre entrée est correcte
         if mot[l] == lettre:
             bool = True
             word_state[int(l)] = lettre
     if bool == True:
-        print(f" Bravo, la lettre '{lettre}' faisait parti du mot !\n")
+        print(f"\n Bravo, la lettre '{lettre}' faisait parti du mot !\n")
     else:
-        print(f" Mince ! la lettre '{lettre}' ne faisait parti du mot, retentez votre chance\n")
+        print(f"\n Mince ! la lettre '{lettre}' ne faisait parti du mot, retentez votre chance\n")
         nombre_de_chances -= 1
     return nombre_de_chances
 
-def lancer_jeu(nombre_de_chance = 6):
+def lancer_jeu(nombre_de_chance = 6):   # Menu
     mot = selectionner_mot()
     word_lenght = len(mot)
     word_state = ["_"] * (word_lenght)
     print(" C'est parti ! ")
     while (nombre_de_chance >= 0):
-        if (nombre_de_chance == 1):
+        if (nombre_de_chance == 1):         # Conditions gérant le nombre de chance restante
             print(" Attention, il vous reste seulement un essai !")
         elif (nombre_de_chance == 0):
-            relancer = input(f" Game over !! Le mot a trouver était : {mot} Appuyer sur entrer pour relancer une partie")
+            relancer = input(f" \nGame over !! Le mot a trouver était : {mot}. Appuyer sur entrer pour relancer une partie")
             lancer_jeu()
         etat = afficher_etat_mot(word_state)
         if (etat == mot):
-            relancer = input(f" Bravo !! Vous avez gagnez ! \n Appuyer sur entrer pour relancer une partie \n")
+            relancer = input(f" \nBravo !! Vous avez gagnez ! Appuyer sur entrer pour relancer une partie \n")
             lancer_jeu()
         lettre = input(" Veuillez choisir une lettre : ")
         nombre_de_chance = verifier_lettre(lettre,mot,word_state,nombre_de_chance)
